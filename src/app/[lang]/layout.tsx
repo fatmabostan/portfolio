5c79fd412130bc1c33d46ff12e88cc8aec8950ec
fatmabostan <fatma_bostan_@outlook.com>
2025-01-14 17:24:42 +0300

    🐛 Fix: Adjusted `params` type in RootLayout to align with `LangParams`

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -2,25 +2,29 @@ import type { Metadata } from "next";
 import { Inter, Calistoga } from "next/font/google";
 import "../globals.css";
 import { twMerge } from "tailwind-merge";
+
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const calistoga = Calistoga({ subsets: ["latin"], variable: "--font-serif", weight: "400" });
+
 export const metadata: Metadata = {
   title: "Fatma Bostan",
   description: "Created with ❤️ in Ankara",
-  
 };
+
 interface LangParams {
   dict: string;
 }
+
 export async function generateStaticParams() {
-  return [{ dict: "en" }, { dict: "tr"}];
+  return [{ dict: "en" }, { dict: "tr" }];
 }
+
 export default function RootLayout({
   children,
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: LangParams;
+  params: Awaited<LangParams>; // Awaited ile çözüm tipi kullanıldı
 }>) {
   return (
     <html lang={params.dict} className="scroll-smooth">
